9653597f997778a0cf4bc9dcded0c2dc0b2fdc04
Sudeep Cholkar <sudeepcholkar918@gmail.com>
2026-04-26 12:45:13 +0530

    variables passing

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("ICEAPPLE","JACKFRUIT","MANGO") #Array
+
+echo "First fruit ${FRUIT[0]}"
+echo "second fruit ${FRUIT[1]}"
+echo "third fruit ${FRUIT[2]}"
+
+echo "all fruits ${FRUIT[@]}"
