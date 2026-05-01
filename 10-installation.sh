0c9bc0da1abf79dc0aa689f40cc6e6328dc6917e
Sudeep Cholkar <sudeepcholkar918@gmail.com>
2026-05-01 13:52:29 +0530

    Installations

--- 10-installation.sh
+++ 10-installation.sh
@@ -15,6 +15,11 @@ if [ $? -ne 0 ]
 then
     echo "Package mysql is not installed. going to install."
     dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Could not proceed with intallation."
+    else 
+        echo "mysql installation success."
 else
     echo "mysql is already installed."
 fi
